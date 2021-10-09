5399f94d1370487e47f4b9c6c874cf56845aedaf
Elijah <82308935+dad-siberian@users.noreply.github.com>
2021-10-09 13:07:15 +0000

    Added the ability to comment on posts

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,10 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 
-
+# Посты блога
 class Post(models.Model):
-    # STATUS_CHOICES = (
-    #     ('draft', 'Draft'),
-    #     ('published', 'Published'),
-    # )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=250, unique_for_date='published_date')
     title = models.CharField(max_length=200)
@@ -28,6 +25,25 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:post_detail',
                         args=[self.slug])
+
+    def title_to_slug(self):
+        return slugify(self.title)
+
+#  Коментарии
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
 
 
 # class Meta:
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,37 @@
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import PostForm
-
+from .forms import PostForm, CommentForm
+from django.template.defaultfilters import slugify
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()        
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+
 
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            post.slug = slugify(request.title)
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
@@ -27,7 +40,9 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, slug):
+    print('post_EDIIT')
     post = get_object_or_404(Post, slug=slug)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
@@ -40,9 +55,3 @@ def post_edit(request, slug):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-    
-
-
-
-
-    
