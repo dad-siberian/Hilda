247dd089e5949331977c2968514f142dd723d812
dad-siberian <dad.siberian@gmail.com>
2021-12-29 16:57:57 +0300

    start front

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,13 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+
     path('', views.post_list, name='post_list'),
-    
+    path('post/', views.post_list, name='post_list'),
     # path('', views.PostListView.as_view(), name='post_list'),         class-based views
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('newpost/', views.post_new, name='post_new'),
+    path('<tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('post/<slug:slug>/edit/', views.post_edit, name='post_edit'),
     
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'taggit',
+    'django_bootstrap_icons'
 ]
 
 MIDDLEWARE = [
